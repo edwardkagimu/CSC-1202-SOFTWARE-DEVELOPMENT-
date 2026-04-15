747a904da11791fc84be20001d0a8e06eda112f9
Chandiga-Siraji <chansonsirajee@gmail.com>
2026-04-15 09:46:04 +0300

    role based access control 2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Auth/Login';
+import Dashboard from './pages/Dashboard/Dashboard';
+import LogForm from './pages/Logs/LogForm';
 
 function App() {
   return (
