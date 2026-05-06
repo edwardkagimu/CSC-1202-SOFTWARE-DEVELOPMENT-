da561dea21fd544eb358d80cd7a1006bd8dcd7d2
Alex jr <elvinalexjrssewajje08@gmail.com>
2026-05-06 08:58:08 +0300

    Fixed deploy errors 2

--- frontend/src/pages/Evaluations/WorkplaceEvaluation.jsx
+++ frontend/src/pages/Evaluations/WorkplaceEvaluation.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useParams } from "react-router-dom";
 import axiosInstance from "../../api/axiosInstance";
-import { useNavigate } from "react-router-dom";
+
 export default function WorkplaceEvaluation() {
 
   const { placementId } = useParams();
