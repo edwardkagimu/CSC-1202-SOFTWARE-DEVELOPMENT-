ea73c6814b2f02a8b8bf338c5aec1156b17bd3bf
Alex jr <elvinalexjrssewajje08@gmail.com>
2026-06-07 15:18:05 +0300

    Enhanced Dashboards and added screenshots

--- frontend/src/pages/SupervisorDashboard.jsx
+++ frontend/src/pages/SupervisorDashboard.jsx
@@ -1,18 +1,56 @@
 import { useNavigate } from "react-router-dom";
+import "../App.css"
 export default function SupervisorDashboard({ stats, user }) {
   const navigate = useNavigate();
-  return (
-    <div style={{ padding: "20px" }}>
-      <h1>Supervisor Dashboard</h1>
+  const totalLogs = stats.pending_logs + stats.reviewed_logs;
 
-      <div>
-        <p>Pending Logs: {stats.pending_logs}</p>
-        <p>Reviewed Logs: {stats.reviewed_logs}</p>
-        <p>Evaluated Students: {stats.evaluated_students}</p>
+  const percentage =
+    totalLogs > 0
+      ? (stats.reviewed_logs / totalLogs) * 100
+      : 0;
+
+  return (
+    <div className="dashboard-container">
+      <div className="dashboard-header">
+        <h1>Supervisor Dashboard</h1>
+        <p>Welcome, {user.username}</p>
       </div>
-      
-      <button onClick={() => navigate("/workplace/logs")}>Review Logs</button>
-      <button onClick={() => navigate("/workplace-students") }>Assigned Students</button>
+      <div className="stats-container">
+        <div className="card">
+         <h3>Pending Logs</h3>
+         <p>{stats.pending_logs}</p>
+        </div>
+
+        <div className="card">
+        <h3>Reviewed Logs</h3>
+        <p>{stats.reviewed_logs}</p>
+        </div>
+
+        <div className="card">
+         <h3>Evaluated Students</h3>
+         <p>{stats.evaluated_students}</p>
+        </div>
+
+        <div className="progress-section">
+         <h2>Log Review Progress</h2>
+
+         <div className="progress-bar">
+          <div
+            className="progress-fill"
+            style={{ width: `${percentage}%` }}
+          ></div>
+         </div>
+
+         <p>{percentage.toFixed(0)}% Reviewed</p>
+        </div>
+      </div>
+
+      <div className="actions-section">
+        <div className="button-group">
+          <button className="action-btn" onClick={() => navigate("/workplace/logs")}>Review Logs</button>
+          <button className="action-btn" onClick={() => navigate("/workplace-students") }>Assigned Students</button>
+        </div>
+      </div>
     </div>
   );
 }
